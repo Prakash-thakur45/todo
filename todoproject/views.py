ad0848c9517147354cbbd6e7819e49b4256fd4f5
Prakash Thakur <prakashthakur72651@gmail.com>
2025-12-11 15:06:24 +0530

    Final code of ToDo

--- todoproject/views.py
+++ todoproject/views.py
@@ -6,8 +6,11 @@ from todo.models import Task
 
 def home(request):
     tasks=Task.objects.filter(isCompleted=False).order_by("-updated_at")
+
+    completed_tasks=Task.objects.filter(isCompleted=True)
+
     context={
-        "tasks":tasks
+        "tasks":tasks,
+        "completed_tasks":completed_tasks,
     }
-    print(tasks)
     return render(request,"index.html",context)
